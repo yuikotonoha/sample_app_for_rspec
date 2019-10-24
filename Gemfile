e082392e7b80e729512e22467e20188a4fa677e8
yuikotonoha <harutinkudo042@gmail.com>
2019-10-24 16:34:35 +0900

    [add]pry-byebugの導入、task_specを試しに書いてみる

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'rspec-rails', '~> 3.8'
   gem 'factory_bot_rails'
+  gem 'pry-byebug'
 end
 
 group :development do
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "titleとstatusを指定しているとき" do
+    it "タスクが作られる" do
+      task = Task.new(title: "kotonoha", status: 0)
+      expect(task).to be_valid
+    end
+  end
+
+  context "titleを指定してない時" do
+    it "エラーが表示" do
+      task = Task.new(status: 0)
+      task.valid?
+      expect(task.errors.messages[:title]).to include "can't be blank"
+    end
+  end
 end
