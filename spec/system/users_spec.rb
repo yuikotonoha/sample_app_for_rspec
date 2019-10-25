f7fe7859971be32e175d84bb72bce71d313c22f3
yuikotonoha <harutinkudo042@gmail.com>
2019-10-25 16:36:14 +0900

    [add][正常系]ユーザーの新規作成できるか、[正常系]ログインできるか、Factorybotにuser用のファイルを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,19 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
-  before do
-    # ユーザを作成する
-    User.create!(email: 'foo@example.com', password: '123456', password_confirmation: "123456")
+
+  describe 'ログイン前' do
+    context '入力値が正常' do
+      it 'ユーザーの新規作成ができる' do
+        # ユーザー作成画面を開く
+        visit new_user_path
+        # 入力フォームにEmailとパスワードと、確認用パスワードを入力する
+        fill_in 'user[email]', with: 'kotonoha@gmail.com'
+        fill_in 'user[password]', with: 'password'
+        fill_in 'user[password_confirmation]', with: 'password'
+        # SignUpボタンをクリックする
+        click_button("SignUp")
+        # ユーザー作成が成功したことを検証する
+        expect(page).to have_content 'User was successfully created.'
+      end
+    end
   end
-  it 'ログインする' do
-    # トップページを開く
-    visit login_path
-    # ログインフォームにEmailとパスワードを入力する
-    fill_in 'email', with: 'foo@example.com'
-    fill_in 'password', with: '123456'
-    # ログインボタンをクリックする
-    click_button("Login")
-    # ログインに成功したことを検証する
-    expect(page).to have_content 'Login successful'
+
+  describe 'ログイン前' do
+    context '入力値が正常' do
+      before do
+        # ユーザを作成する
+        create(:user)
+      end
+      it 'ログインできる' do
+        # トップページを開く
+        visit login_path
+        # ログインフォームにEmailとパスワードを入力する
+        fill_in 'email', with: 'kotonoha@gmail.com'
+        fill_in 'password', with: 'password'
+        # ログインボタンをクリックする
+        click_button("Login")
+        # ログインに成功したことを検証する
+        expect(page).to have_content 'Login successful'
+      end
+    end
   end
+
+      # it 'ログインできる' do
+      #   # プロフィール編集を開く
+      #   visit edit_user_path(@user)
+      #   # ログインフォームにEmailとパスワードを入力する
+      #   fill_in 'user[email]', with: 'kotonoha2@gmail.com'
+      #   fill_in 'user[password]', with: 'password2'
+      #   fill_in 'user[password_confirmation]', with: 'password2'
+      #   # Updateボタンをクリックする
+      #   click_button("Update")
+      #   # プロフィール更新に成功したことを検証する
+      #   expect(page).to have_content 'User was successfully updated.'
+      # end
+
+
 end
+
