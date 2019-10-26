a557d9ef7f28ab2bf50b4000cfdaf09a34f56296
yuikotonoha <harutinkudo042@gmail.com>
2019-10-26 12:29:55 +0900

    [add][異常系]フォーム未入力時にログインが失敗する

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
+
   describe 'ログイン前' do
     before do
       # ユーザを作成する
-      @user = create(:user)
+      create(:user)
     end
     context '入力値が正常' do
       it 'ログインが成功する' do
@@ -17,6 +18,26 @@ RSpec.describe 'Users', type: :system do
         click_button("Login")
         # ログインに成功したことを検証する
         expect(page).to have_content 'Login successful'
+      end
+    end
+  end
+
+  describe 'ログイン前' do
+    before do
+      # ユーザを作成する
+      create(:user)
+    end
+    context 'フォーム未入力時に' do
+      it 'ログインが失敗する' do
+        # トップページを開く
+        visit login_path
+        # ログインフォームにEmailとパスワードを入力する
+        fill_in 'email', with: ''
+        fill_in 'password', with: ''
+        # ログインボタンをクリックする
+        click_button("Login")
+        # ログインに成功したことを検証する
+        expect(page).to have_content 'Login failed'
       end
     end
   end
