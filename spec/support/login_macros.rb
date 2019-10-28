2489984a6c53ca1f941f064d21ba90aba6cecfa6
yuikotonoha <harutinkudo042@gmail.com>
2019-10-28 12:07:02 +0900

    [modify]シングルクォートに修正

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -3,7 +3,7 @@ module LoginMacros
     visit login_path
     fill_in 'email', with: user.email
     fill_in 'password', with: 'password'
-    click_button("Login")
+    click_button('Login')
     expect(page).to have_content 'Login successful'
   end
 end
